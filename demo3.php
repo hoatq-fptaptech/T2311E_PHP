8b14f6c9b2a87841ec59972be85beb0f90e6f07f
quanghoatrinh <quanghoa.trinh@gmail.com>
2024-04-23 20:42:36 +0700

    demo params

--- demo3.php
+++ demo3.php
@@ -11,7 +11,10 @@
       die("Connect database failed");
     }
   //2. query SQL
-    $sql = "SELECT * FROM products";
+     // 2.1. Lấy tham số
+     $limit = isset($_GET["limit"]) && $_GET["limit"]!= "" ?$_GET["limit"]:20;
+     //2.2. áp dụng giá trị tham số vào truy vấn
+    $sql = "SELECT * FROM products LIMIT $limit";
     $result = $conn->query($sql);
     $list = [];
     while($row = $result->fetch_assoc()){
@@ -29,7 +32,22 @@
 
 </head>
 <body>
-    <div class="container">
+  <div class="container">
+    <form action="/demo3.php" method="GET">
+        <div class="row">
+          <div class="col">
+          <input name="search" placeholder="Search" type="text" class="form-control"/>
+          </div>
+          <div class="col">
+            <input value="<?php echo $limit; ?>" name="limit" placeholder="Limit" type="number" class="form-control"/>
+          </div>
+          <div class="col">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </div>
+    </form>
+  </div>
+  <div class="container">
     <table class="table">
   <thead>
     <tr>
