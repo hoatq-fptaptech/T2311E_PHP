dc15c358fb37203d3f3d1e716a67a7fab9d2a729
quanghoatrinh <quanghoa.trinh@gmail.com>
2024-05-02 19:35:53 +0700

    home with function php

--- index.php
+++ index.php
@@ -1,27 +1,46 @@
 <?php
-// code here
-// echo "Hello world!";
-$x = 10;// number
-$s = "Hello"; // string
-$t = true;// boolean
-echo $s.$x;
-
-if($x > 5){
-
-}else{
-
-}
-for($i=0;$i<10;$i++){
-
-}
-$x = "ABC";
-
-function tinh_tong($a,$b){
-    return $a+$b;
-}
-function tinh_hieu($a,$b){
-    $c = $a-$b;
-    echo $c;
-}
-$z = tinh_tong(5,7);
-tinh_hieu(7,5);
+    require_once("functions/product.php");
+    $newest_products = newest_products();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php include_once("html/styles.php");?>
+</head>
+<body>
+    <header>
+    </header>
+    <?php include_once("html/nav.php");?>
+    <main class="main">
+        <div class="container">
+            <h2>Newest products</h2>
+            <div class="row">
+                <?php foreach($newest_products as $item):?>
+                    <div class="col-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="<?php echo $item["thumbnail"] ?>" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $item["name"] ?></h5>
+                                <p class="card-text"><?php echo $item["description"] ?></p>
+                                <p class="text-danger"><?php echo $item["price"] ?></p>
+                                <a href="#" class="btn btn-primary">Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach;?>    
+            </div>
+        </div>
+        <div class="container">
+            <h2>Best sellers</h2>
+            
+        </div>
+        <div class="container">
+            <h2>Hot items</h2>
+            
+        </div>
+    </main>
+</body>
+</html>
