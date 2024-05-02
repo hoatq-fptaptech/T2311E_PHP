2a5bdb617704de96183bcab50c5098cc1358c217
quanghoatrinh <quanghoa.trinh@gmail.com>
2024-05-02 21:03:19 +0700

    demo category

--- functions/product.php
+++ functions/product.php
@@ -9,3 +9,50 @@ function newest_products(){
     }
     return $list;
 }
+function best_sellers(){
+    $sql = "select * from products order by qty desc limit 4";
+    $result = query($sql);
+    $list = [];
+    while($row = $result->fetch_assoc()){
+        $list[] = $row;
+    }
+    return $list;
+}
+
+function hot_items(){
+    $sql = "select * from products order by price desc limit 4";
+    $result = query($sql);
+    $list = [];
+    while($row = $result->fetch_assoc()){
+        $list[] = $row;
+    }
+    return $list;
+}
+
+function categories_all(){
+    $sql = "select * from categories";
+    $result = query($sql);
+    $list = [];
+    while($row = $result->fetch_assoc()){
+        $list[] = $row;
+    }
+    return $list;
+}
+
+function category_detail($category_id){
+    $sql_cat = "select * from categories where id = $category_id";
+    $result = query($sql_cat);
+    if($result->num_rows > 0){
+        $category = $result->fetch_assoc();
+        $sql_product = "select * from products where category_id = $category_id";
+        $result = query($sql_product);
+        $list = [];
+        while($row = $result->fetch_assoc()){
+            $list[] = $row;
+        }
+        $category["products"] = $list;
+        return $category;
+    }
+    return null;
+    
+}
