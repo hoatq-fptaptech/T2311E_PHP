c10217efaf54176d611ad5e497bc6baabd07c756
quanghoatrinh <quanghoa.trinh@gmail.com>
2024-05-07 21:21:58 +0700

    place order

--- functions/database.php
+++ functions/database.php
@@ -14,3 +14,11 @@ function query($sql){
     $conn = connect();
     return $conn->query($sql);
 }
+
+function insert_get_id($sql){
+    $conn = connect();
+    if($conn->query($sql) == true){
+        return $conn->insert_id;
+    }
+    return 0;
+}
